329628ad71d0755d38b52806f2f3e38684fe6e70
Christian Woolf <cwoolf210799@gmail.com>
2022-02-28 19:54:16 -0600

    entrega DOM

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 
-let repetir=true;
+let formh=document.getElementById('formh')
+let Stotal=document.getElementById('STotal')
 let total=0;
 const suma= (a,b) => a+b;
 let id=0;
-let tamano=0;
 let array=[];
-alert("Instrucciones:Ingrese los numeros de lo que desea ordenar")
-while(repetir){
+formh.addEventListener('submit', (e) =>{
+    e.preventDefault()
     id=id+1;
-    let producto=parseInt(prompt("Que desea ordenar? 1.-Hamburgesa sencilla $25 2.-Hamburguesa con queso $35 3.-Sandwich de pollo $30"))
+    let producto=document.getElementById('producto').value
+    let tamano=document.getElementById('tamano').value
+    formh.reset()
     if (producto==1){
         total=suma(total,25)
      } else{
@@ -18,34 +20,27 @@ while(repetir){
               if (producto==3){
                 total=suma(total,30)
             } else{
-                    alert("Número no válido")
+                    alert("Número de producto no válido")
                 }  
             }
         }
-        if (producto==1 || producto==2 || producto==3){
-            tamano=parseInt(prompt("Tamaño? 1.-Chico $5 2.-Mediano $10 3.-Grande $15"))
-            if (tamano==1){
-                total=suma(total,5) 
+        if (tamano==1){
+            total=suma(total,5) 
+         } else{
+            if (tamano==2){
+                total=suma(total,10)
             } else{
-                if (tamano==2){
-                    total=suma(total,10)
+                if (tamano==3){
+                    total=suma(total,15)
                 } else{
-                    if (tamano==3){
-                        total=suma(total,15)
-                    }
-                }
+                    alert("Número de combo no válido")
+                }  
             }
         }
         let orden = {id,producto,tamano,total};
         array.push(orden)
-    let confirmacion = prompt("¿Desea pedir otra cosa? si o no")
-    if(confirmacion == "no") {
-        repetir = false
-    }
-}
-alert(`Tu total es de ${total}`)
-/* impresion en la consola de cuantas y que hamburguesas se ordenaron */
-for (const producto of array){
-    console.log(producto.producto)
-}
+        
+        Stotal.innerHTML = `<h2> Tu total es de ${total}</h2>`
+})
+
 console.log(array)
